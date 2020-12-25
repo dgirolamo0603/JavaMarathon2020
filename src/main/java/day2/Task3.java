541f81694fc76b31c288792c7414c85642c0ed27
Aleksey Petrov <dgirolamo777@gmail.com>
2020-12-25 10:19:53 +0300

    Add Task2 solution

--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,23 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
 
+        if (a < b) {
+            int i=a;
+            while (i >= a && i <= b) {
+                if (i % 5 == 0 && i % 10 != 0) {
+                    System.out.print(i + " ");
+                }
+                i++;
+            }
+        } else if (a > b) {
+            System.out.println("Некорректный ввод");
+        }
     }
 }
