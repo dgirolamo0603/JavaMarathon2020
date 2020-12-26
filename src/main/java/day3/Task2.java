f3a6a76ec3e671c743e6ba28f7734e9292dfc595
Aleksey Petrov <dgirolamo777@gmail.com>
2020-12-26 11:13:49 +0300

    Add solution Day3

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,19 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            double x = scanner.nextDouble();
+            double y = scanner.nextDouble();
+            System.out.printf("%.0f %.0f - ваш ввод в консоль \n", x, y);
+            if (y == 0) break;
+            double res = x / y;
+            System.out.println(res + " - ответ программы");
 
+
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,22 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            double x = scanner.nextDouble();
+            double y = scanner.nextDouble();
+            System.out.printf("%.0f %.0f - ваш ввод в консоль \n", x, y);
+            if (y == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            double res = x / y;
+            System.out.println(res + " - ответ программы");
 
+
+        }
     }
 }
