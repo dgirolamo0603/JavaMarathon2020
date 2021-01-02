17738ccea620e66bb500529e9447d276d163d460
Aleksey Petrov <dgirolamo777@gmail.com>
2021-01-02 14:48:38 +0300

    Add solution Day 8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -1,7 +1,33 @@
 package day8;
 
+import java.util.Date;
+
 public class Task1 {
     public static void main(String[] args) {
+        StringBuilder s = new StringBuilder();
+        Date currentTime = new Date();
 
+        for (int i = 0; i <= 20000; i++) {
+            s.append(i + " ");
+        }
+
+        System.out.println(s);
+
+        Date newTime = new Date();
+        long msDelay = newTime.getTime() - currentTime.getTime();
+
+
+        Date currentTimeS = new Date();
+        String s1 = "";
+        for (int i = 0; i <= 20000; i++) {
+            s1 += (i + " ");
+        }
+        System.out.println(s1);
+
+        Date newTimeS = new Date();
+        long msDelay1 = newTimeS.getTime() - currentTimeS.getTime();
+
+        System.out.println(msDelay);
+        System.out.println(msDelay1);
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -3,5 +3,11 @@ package day8;
 public class Task2 {
     public static void main(String[] args) {
 
+        Airplane airplane = new Airplane("Boeing", 2000, 150, 10000);
+
+        System.out.printf("Изготовитель: %s, год выпуска: %d, длина: %d, вес: %d, количество топлива в баке: %d", airplane.getProducer(),
+                airplane.getYear(), airplane.getLength(), airplane.getWeight(), airplane.getFuel());
+
+
     }
 }
